0599c5332da1774a4d1e19c4c4ea59a8be00b872
Sticmac <jujulem06@hotmail.fr>
2016-04-20 11:02:57 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+//Redirection immédiate vers le fichier labyrinthe.php
+header('Location: labyrinthe.php');
+
+?>
---
+++ labyrinthe.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>Laby</title>
+</head>
+<body>
+	<h1>Bonjoooooour :3</h1>
+</body>
+</html>
