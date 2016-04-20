45ff9e4bfa5937eed07ef51d537fb25f5e4cf9ef
Sticmac <jujulem06@hotmail.fr>
2016-04-20 20:31:09 +0200

    JSON working. TODO: Build map from it.

--- js/map.js
+++ js/map.js
@@ -32,12 +32,7 @@ function createGrid() {
 	for (var x = 0 ; x < lines ; x++) {
 		table += "<tr>\n";
 		for (var y = 0 ; y < columns ; y++) {
-			if (x == 0) {
-				table += "<td style='padding: "+lenCase+"px; background-color: "+getBackgroundFromElement("wall")+";' class='wall' id='"+x+","+y+"'></td>";
-			}
-			else {
-				table += "<td style='padding: "+lenCase+"px; background-color: "+getBackgroundFromElement()+";' id='"+x+","+y+"'></td>";
-			}
+			table += "<td style='padding: "+lenCase+"px; background-color: "+getBackgroundFromElement()+";' id='"+x+","+y+"'></td>";
 		}
 		table += "</tr>\n";
 	}
