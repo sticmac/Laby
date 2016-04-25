4ae177bfaba3dfa55aa387780ec5bfc2274d36ea
Sticmac <jujulem06@hotmail.fr>
2016-04-25 22:06:12 +0200

    Character added

--- js/map.js
+++ js/map.js
@@ -4,8 +4,10 @@
 var lines = 10;
 var columns = 15;
 var lenCase = 0;
-var character = new Character(0,0,5,7);
-//var character = [0, 0, 5, 7];
+var character;
+
+var lastCase = [0,0];
+var lastRoom = [0,0];
 
 //Objet Character
 //Paramêtres : xS, yS : coordonnées de la salle de départ
@@ -14,7 +16,6 @@ function Character(xS, yS, xC, yC) {
 	this.room = [xS, yS];
 	this.roomCase = [xC, yC];
 }
-
 
 function getMapPHP() {
 	var xhr = new XMLHttpRequest();
@@ -40,13 +41,23 @@ function createGrid(map) {
 	for (var y = 0 ; y < lines ; y++) {
 		table += "<tr>\n";
 		for (var x = 0 ; x < columns ; x++) {
-			var classname = map[0].cases[y][x].nature;
+			var classname = map[0].cases[x][y].nature;
 			table += "<td class='"+classname+"' style='padding: "+lenCase+"px; background-color: "+getBackgroundFromElement(classname)+";' id='"+x+","+y+"'></td>";
 		}
 		table += "</tr>\n";
 	}
 	table += "</table>\n";
 	document.getElementById("laby").innerHTML = table;
+	document.getElementById(5+","+7).classList.add("char");
+}
+
+function update() {
+	for (var i = 0 ; i < columns ; i++) {
+		for (var j = 0 ; j < lines ; j++) {
+			var elt = document.getElementById(i+","+j);
+			elt.style["background-color"] = getBackgroundFromElement(elt.classList[0]);
+		}
+	}
 }
 
 function changeSizes() {                                                                              
@@ -68,13 +79,65 @@ function getBackgroundFromElement(classname) {
 		case "wall":
 			return "black";
 			break;
+		case "char":
+			return "red";
+			break;
 		default:
 			return "green";
 			break;
 	}
 }
 
+function changePos(evt) {
+	var key = evt.which || evt.keyCode;
+	document.getElementById(lastCase[0]+','+lastCase[1]).classList.remove("char");
+	switch (key) {
+		case 37:
+		case 81:
+			//Flèche gauche ou Q
+			if (canMove(character.roomCase[0]-1, character.roomCase[1])) {
+				character.roomCase[0]--;
+			}
+			break;
+		case 38:
+		case 90:
+			//Flèche haut ou Z
+			if (canMove(character.roomCase[0], character.roomCase[1]-1)) {
+				character.roomCase[1]--;
+			}
+			break;
+		case 39:
+		case 68:
+			//Flèche droite ou D
+			if (canMove(character.roomCase[0]+1, character.roomCase[1])) {
+				character.roomCase[0]++;
+			}
+			break;
+		case 40:
+		case 83:
+			//Flèche bas ou S
+			if (canMove(character.roomCase[0], character.roomCase[1]+1)) {
+				character.roomCase[1]++;
+			}
+			break;
+	}
+	lastCase[0] = character.roomCase[0];
+	lastCase[1] = character.roomCase[1];
+	document.getElementById(lastCase[0]+','+lastCase[1]).classList.add("char");
+}
+
+function canMove(x, y) {
+	if(x < columns && y < lines && x >= 0 && y >= 0
+			&& document.getElementById(x+','+y).className != "wall") {
+		return true;
+	}
+	else return false;
+}
+
 function go() {
 	changeSizes();
 	getMapPHP();
+	character = new Character(0,0,5,7);
+	lastCase = [5,7];
+	setInterval(update, 100);
 }
--- save.php
+++ save.php
@@ -3,17 +3,17 @@
 header("Content-type: text/json");
 
 echo "[{\"cases\" : [";
-for ($y = 0 ; $y < 10 ; $y++) {
+for ($x = 0 ; $x < 15 ; $x++) {
 	echo "[";
-	for ($x = 0 ; $x < 15 ; $x++) {
+	for ($y = 0 ; $y < 10 ; $y++) {
 		echo "{\"nature\" : ";
 		if ($x == 0 || $y == 0 || $x == 14 || $y == 9) { echo "\"wall\""; }
 		else { echo "\"\""; }
 		echo "}";
-		if ($x != 14) { echo ", "; }
+		if ($y != 9) { echo ", "; }
 	}
 	echo "]";
-	if ($y != 9) { echo ", "; }
+	if ($x != 14) { echo ", "; }
 }
 echo "]}";
 echo "]";
