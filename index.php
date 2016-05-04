907cda83babe510d142b5332dfa31a5df21e1dc8
Sticmac <jujulem06@hotmail.fr>
2016-05-04 07:59:00 +0200

    Login fully workingvim login.php vim login.php vim login.php vim login.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php 
+include_once("inc/nav.inc"); ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,19 +8,7 @@
 	<link type="text/css" rel="stylesheet" href="./css/bootstrap.min.css" />
 </head>
 <body>
-	<nav class ="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-			<a class="navbar-brand" href="./index.php">Laby</a>
-		</div>
-		<div class="collapse navbar-collapse">
-			<ul class="nav navbar-nav">
-				<li><a href="./labyrinthe.php">Labyrinthe</a></li>
-				<li><a href="./editor.php">Editeur</a></li>
-			</ul>
-			<ul class="nav navbar-nav navbar-right">
-                                <li><a href='login.php'>Log in</a></li>
-                        </ul>
-		</div>
-	</nav>
+<?php navbar(); ?>
+
 </body>
+</html>
