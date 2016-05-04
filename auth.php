30a6e461e94e2d1e73511acd1ad6a1309f7c0386
Sticmac <jujulem06@hotmail.fr>
2016-05-04 21:48:43 +0200

    Subscribe page created : can add users

--- auth.php
+++ auth.php
@@ -2,18 +2,28 @@
 session_start();
 include_once('inc/users.inc');
 
-$username = $_POST['user'];
-$password = $_POST['password'];
-
-$xml = simplexml_load_file("xml/users.xml");
-
-if (isset($_GET['action']) && $_GET['action'] == "logout") { //Si on se deconnecte
-	logout();
-	header("Location: index.php");
-	exit;
+if (isset($_GET['action'])) { //Si on se deconnecte
+	if ($_GET['action'] == "logout") { 
+		logout();
+		header("Location: index.php");
+		exit;
+	}
+	else if ($_GET['action'] == "subscribe") {
+		if(subscribe($_POST["user"], md5($_POST["password1"]))) {
+			header("Location: index.php");
+			exit;
+		}
+		else {
+			header("Location: subscribe.php#refused");
+			exit;
+		}
+	}
 }
 else {
-	if (login($username, md5($password))) {
+	$username = $_POST['user'];
+	$password = $_POST['password'];
+
+	if(login($username, md5($password))) { 
 		header("Location: index.php");
 		exit;
 	}
