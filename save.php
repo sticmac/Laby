e29bf610cd597d426cd7ccd3d503ce389430113c
Sticmac <jujulem06@hotmail.fr>
2016-05-04 13:07:09 +0200

    Merge with Tordi

--- save.php
+++ save.php
@@ -1,20 +1,11 @@
 <?php
 
+if (!empty($_GET) && !empty($_GET[""]))
 header("Content-type: text/json");
 
-echo "[{\"cases\" : [";
-for ($x = 0 ; $x < 15 ; $x++) {
-	echo "[";
-	for ($y = 0 ; $y < 10 ; $y++) {
-		echo "{\"nature\" : ";
-		if ($x == 0 || $y == 0 || $x == 14 || $y == 9) { echo "\"wall\""; }
-		else { echo "\"ground\""; }
-		echo "}";
-		if ($y != 9) { echo ", "; }
-	}
-	echo "]";
-	if ($x != 14) { echo ", "; }
-}
-echo "]}";
-echo "]";
+$xml = simplexml_load_file("xml/maps.xml");
+$maps = $xml->xpath("map");
+echo (string)$maps[0];
+
+
 ?>
