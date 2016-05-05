a7c6d6dc63f92d16e0c12cfd4d6d2681c0b077af
Sticmac <jujulem06@hotmail.fr>
2016-05-05 10:45:12 +0200

    Sign up and log in finished

--- index.php
+++ index.php
@@ -6,9 +6,21 @@ include_once("inc/nav.inc"); ?>
         <title>Laby - Accueil</title>
 	<script src="js/map.js"></script>
 	<link type="text/css" rel="stylesheet" href="./css/bootstrap.min.css" />
+	<link type="text/css" rel="stylesheet" href="./css/laby.css" />
 </head>
 <body>
 <?php navbar(); ?>
-
+<div class="container">
+	<div class="row">
+		<div id="signedup" class="col-md-offset-3 col-md-6 alert alert-success text-center">
+			<strong>Inscription réalisée avec succès. <br /> Vous pouvez maintenant vous connecter.</strong>
+		</div>
+	</div>
+	<div class="row">
+		<div id="loggedin" class="col-md-offset-3 col-md-6 alert alert-success text-center">
+			<strong>Authentification réussie.</strong>
+		</div>
+	</div>
+</div>
 </body>
 </html>
